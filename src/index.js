33055e3aed476bb89ad652de2597855966253b5c
Muñoz Barrio, Juan Luis <juan.c.munoz@accenture.com>
2024-07-25 09:54:04 +0200

    Hola

--- src/index.js
+++ src/index.js
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { I18nextProvider } from 'react-i18next';
+
+import global_es from'./traducciones/ES/global.json';
+
+import global_en from'./traducciones/EN/global.json';
+
+import i18next, { init } from 'i18next';
+
+i18next: init({
+  interpolation: { escapeValue: false },
+  lng: "es",
+  resources: {
+    es: {
+      global: global_es
+    },
+    en: {
+      global: global_en
+    }
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
