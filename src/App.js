8c7f475fcb4fbac232ef8bd9d80122c8763dea6d
JuanLu <juanlumunoz7@gmail.com>
2024-07-30 13:30:36 +0200

    Hello

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import "./i18n" ;
 import Header from './components/header';
 
 function App() {
--- src/i18n.js
+++ src/i18n.js
@@ -1,23 +1,25 @@
+// i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend'; // Para cargar traducciones desde archivos
-import LanguageDetector from 'i18next-browser-languagedetector'; // Para detectar idioma del navegador
+import enTranslations from './traducciones/EN/global.json';
+import esTranslations from './traducciones/ES/global.json';
 
 i18n
-  .use(Backend)
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en', // Idioma por defecto
-    debug: true, // Opcional: para ver logs en la consola
-
+    resources: {
+      en: {
+        translation: enTranslations
+      },
+      es: {
+        translation: esTranslations
+      }
+    },
+    lng: 'es', // idioma por defecto
+    fallbackLng: 'es',
     interpolation: {
-      escapeValue: false, // No escapar valores para evitar problemas con React
-    },
-    backend: {
-      // Cargar traducciones desde archivos estáticos
-      loadPath: '/components/header.js',
-    },
+      escapeValue: false
+    }
   });
 
 export default i18n;
