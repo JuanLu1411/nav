cb73db7587f8690ea3217d93c5706be66d90616b
JuanLu <juanlumunoz7@gmail.com>
2024-07-31 11:36:26 +0200

    Hello

--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,4 @@
-// i18n.js
+/* // i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import enTranslations from './traducciones/EN/global.json';
@@ -16,6 +16,33 @@ i18n
       }
     },
     lng: 'es', // idioma por defecto
+    fallbackLng: 'es',
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
+*/
+
+// src/i18n.js
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import enTranslations from './traducciones/EN/global.json';
+import esTranslations from './traducciones/ES/global.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: enTranslations
+      },
+      es: {
+        translation: esTranslations
+      }
+    },
+    lng: 'es',
     fallbackLng: 'es',
     interpolation: {
       escapeValue: false
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -38,3 +38,20 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
